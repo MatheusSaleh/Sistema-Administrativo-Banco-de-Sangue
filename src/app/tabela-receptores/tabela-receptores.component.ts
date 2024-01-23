ec2447da3ec5de74cd562f635646612e2a66c455
MatheusSaleh <matheusxmg2@gmail.com>
2024-01-23 08:23:00 -0300

    Adicionado a tabela de receptores

--- src/app/tabela-receptores/tabela-receptores.component.ts
+++ src/app/tabela-receptores/tabela-receptores.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { ReceptorService } from '../services/receptor.service';
+import { Receptor } from '../models/Receptor.model';
+import { TableModule } from 'primeng/table';
+import { MatButtonModule } from '@angular/material/button';
+import { TooltipModule } from 'primeng/tooltip';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-tabela-receptores',
   standalone: true,
-  imports: [],
+  imports: [TableModule, MatButtonModule, TooltipModule, ProgressSpinnerModule, NgIf],
   templateUrl: './tabela-receptores.component.html',
   styleUrl: './tabela-receptores.component.css'
 })
 export class TabelaReceptoresComponent {
+
+  constructor(private receptorService: ReceptorService){}
+
+  public receptores: Receptor[] = [];
+
+  public carregandoReceptores: boolean = false;
+
+  ngOnInit(): void{
+    this.buscarReceptores();
+  }
+
+  public buscarReceptores(): void{
+    this.carregandoReceptores = true;
+
+    this.receptorService.listarReceptores().subscribe((dados: Receptor[]) => {
+      this.receptores = dados;
+      this.carregandoReceptores = false;
+      console.log(this.receptores);
+    })
+  }
 
 }
